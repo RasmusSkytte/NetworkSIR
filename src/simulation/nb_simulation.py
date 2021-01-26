d691e408f42b636f42ada5464fcda2963113bc06
ESMartiny <Emil.s.martiny@gmail.com>
2021-01-26 09:01:09 +0100

    better init and correct ages
    init from a better dist. Fixed a logical error in household init, it odes now gives the correct age distriubiton and household size distribution, matching the national levels.

--- src/simulation/nb_simulation.py
+++ src/simulation/nb_simulation.py
@@ -682,7 +682,7 @@ def place_and_connect_families(
     people_index_to_value = np.arange(1, N_dim_people_in_household + 1)
 
     #initialize lists to keep track of number of agents in each age group
-    counter_ages = np.zeros(N_ages, dtype=np.uint16)
+    counter_ages = np.zeros(N_ages, dtype=np.uint32)
     agents_in_age_group = utils.initialize_nested_lists(N_ages, dtype=np.uint32)
 
     mu_counter = 0
@@ -768,14 +768,13 @@ def place_and_connect_families_kommune_specific(
     #initialize lists to keep track of number of agents in each age group
     counter_ages = np.zeros(N_ages, dtype=np.uint32)
     agents_in_age_group = utils.initialize_nested_lists(N_ages, dtype=np.uint32)
-
+    house_sizes = np.zeros(len(people_index_to_value), dtype=np.int64)    
     mu_counter = 0
     agent = 0
     do_continue = True
     while do_continue:
 
         agent0 = agent
-
         house_index = all_indices[agent]
         coordinates = coordinates_raw[house_index]
         kommune = Kommune_ids[house_index]
@@ -784,6 +783,7 @@ def place_and_connect_families_kommune_specific(
         people_in_household_kom = people_in_household[kommune,:]
         N_people_in_house_index = utils.rand_choice_nb(people_in_household_kom)
         N_people_in_house = people_index_to_value[N_people_in_house_index]
+        house_sizes[N_people_in_house_index] += 1
         # if N_in_house would increase agent to over N_tot,
         # set N_people_in_house such that it fits and break loop
         if agent + N_people_in_house >= N_tot:
@@ -825,7 +825,7 @@ def place_and_connect_families_kommune_specific(
                     mu_counter += 1
 
     agents_in_age_group = utils.nested_lists_to_list_of_array(agents_in_age_group)
-
+    print(house_sizes)            
     return mu_counter, counter_ages, agents_in_age_group
 
 @njit
@@ -1017,13 +1017,70 @@ def nb_random_choice(arr, prob, size=1, replace=False):
                 out.add(x)
         return set_to_array(out)
 
-
+@njit
+def exp_func(x, a, b, c):
+  
+    return a * np.exp(b * x) + c
 @njit
 def make_random_initial_infections(my, possible_agents):
-    if my.cfg.weighted_random_initial_infections:
+    if my.cfg.weighted_random_initial_infections:       
+        probs =np.array([7.09189651e+00, 7.21828639e+00, 7.35063322e+00, 7.48921778e+00,
+           7.63433406e+00, 7.78628991e+00, 7.94540769e+00, 8.11202496e+00,
+           8.28649517e+00, 8.46918845e+00, 8.66049237e+00, 8.86081276e+00,
+           9.07057458e+00, 9.29022282e+00, 9.52022345e+00, 9.76106439e+00,
+           1.00132566e+01, 1.02773350e+01, 1.05538598e+01, 1.08434177e+01,
+           1.11466230e+01, 1.14641189e+01, 1.17965788e+01, 1.21447081e+01,
+           1.25092454e+01, 1.28909638e+01, 1.32906734e+01, 1.37092218e+01,
+           1.41474972e+01, 1.46064292e+01, 1.50869914e+01, 1.55902033e+01,
+           1.61171325e+01, 1.66688966e+01, 1.72466664e+01, 1.78516673e+01,
+           1.84851830e+01, 1.91485573e+01, 1.98431976e+01, 2.05705774e+01,
+           2.13322397e+01, 2.21298006e+01, 2.29649517e+01, 2.38394649e+01,
+           2.47551954e+01, 2.57140858e+01, 2.67181702e+01, 2.77695789e+01,
+           2.88705421e+01, 3.00233956e+01, 3.12305849e+01, 3.24946711e+01,
+           3.38183358e+01, 3.52043869e+01, 3.66557650e+01, 3.81755489e+01,
+           3.97669627e+01, 4.14333825e+01, 4.31783434e+01, 4.50055472e+01,
+           4.69188701e+01, 4.89223712e+01, 5.10203005e+01, 5.32171087e+01,
+           5.55174561e+01, 5.79262227e+01, 6.04485185e+01, 6.30896942e+01,
+           6.58553529e+01, 6.87513617e+01, 7.17838642e+01, 7.49592935e+01,
+           7.82843861e+01, 8.17661957e+01, 8.54121088e+01, 8.92298599e+01,
+           9.32275478e+01, 9.74136534e+01, 1.01797057e+02, 1.06387058e+02,
+           1.11193393e+02, 1.16226259e+02, 1.21496331e+02, 1.27014792e+02,
+           1.32793346e+02, 1.38844253e+02, 1.45180349e+02, 1.51815076e+02,
+           1.58762509e+02, 1.66037386e+02, 1.73655140e+02, 1.81631931e+02,
+           1.89984681e+02, 1.98731110e+02, 2.07889773e+02, 2.17480099e+02,
+           2.27522433e+02, 2.38038079e+02, 2.49049344e+02, 2.60579589e+02,
+           2.72653273e+02, 2.85296010e+02, 2.98534620e+02, 3.12397188e+02,
+           3.26913121e+02, 3.42113214e+02, 3.58029713e+02, 3.74696383e+02,
+           3.92148580e+02, 4.10423328e+02, 4.29559396e+02, 4.49597378e+02,
+           4.70579783e+02, 4.92551123e+02, 5.15558010e+02, 5.39649249e+02,
+           5.64875947e+02, 5.91291622e+02, 6.18952312e+02, 6.47916695e+02,
+           6.78246218e+02, 7.10005221e+02, 7.43261078e+02, 7.78084340e+02,
+           8.14548879e+02, 8.52732052e+02, 8.92714861e+02, 9.34582126e+02,
+           9.78422665e+02, 1.02432948e+03, 1.07239996e+03, 1.12273608e+03,
+           1.17544463e+03, 1.23063741e+03, 1.28843153e+03, 1.34894957e+03,
+           1.41231994e+03, 1.47867705e+03, 1.54816168e+03, 1.62092124e+03,
+           1.69711008e+03, 1.77688982e+03, 1.86042971e+03, 1.94790698e+03,
+           2.03950719e+03, 2.13542468e+03, 2.23586291e+03, 2.34103497e+03,
+           2.45116395e+03, 2.56648350e+03, 2.68723825e+03, 2.81368437e+03,
+           2.94609011e+03, 3.08473635e+03, 3.22991721e+03, 3.38194069e+03,
+           3.54112929e+03, 3.70782070e+03, 3.88236856e+03, 4.06514315e+03,
+           4.25653221e+03, 4.45694175e+03, 4.66679693e+03, 4.88654292e+03,
+           5.11664591e+03, 5.35759404e+03, 5.60989844e+03, 5.87409437e+03,
+           6.15074229e+03, 6.44042909e+03, 6.74376930e+03, 7.06140644e+03,
+           7.39401435e+03, 7.74229861e+03, 8.10699809e+03, 8.48888646e+03,
+           8.88877386e+03, 9.30750861e+03, 9.74597902e+03, 1.02051153e+04,
+           1.06858914e+04, 1.11893272e+04, 1.17164909e+04, 1.22685006e+04,
+           1.28465275e+04, 1.34517977e+04, 1.40855953e+04, 1.47492649e+04,
+           1.54442143e+04, 1.61719178e+04, 1.69339191e+04, 1.77318349e+04,
+           1.85673577e+04, 1.94422602e+04, 2.03583982e+04, 2.13177153e+04,
+           2.23222466e+04, 2.33741232e+04, 2.44755764e+04, 2.56289429e+04])
+        
+        proba = np.zeros(my.cfg.N_tot,dtype=np.float64)
+        for agent in range(my.cfg.N_tot):
+            proba[agent] = probs[my.number_of_contacts[agent]]
         return nb_random_choice(
             possible_agents,
-            prob=my.number_of_contacts,
+            prob=proba,
             size=my.cfg.N_init,
             replace=False,
         )
@@ -1121,6 +1178,22 @@ def find_outbreak_agent(my, possible_agents, coordinate, rho, max_tries=10_000):
 
 
 @njit
+def calc_E_I_dist(my, r_guess):
+    p_E = 4/my.cfg.lambda_E
+    p_I = 4/my.cfg.lambda_I
+    gen = p_E + p_I
+    E_I_weight_list = np.ones(8,dtype=np.float64)
+    for i in range(1,4):
+        E_I_weight_list[i:] = E_I_weight_list[i:]* r_guess**(1/my.cfg.lambda_I/(p_E*p_I))
+    for i in range(4,8):
+        E_I_weight_list[i:] = E_I_weight_list[i:]* r_guess**(1/my.cfg.lambda_E/(p_E*p_I))
+    E_I_weight_list = E_I_weight_list[::-1]    
+    return E_I_weight_list     
+
+
+
+
+@njit
 def make_initial_infections(
     my,
     g,
@@ -1148,7 +1221,9 @@ def make_initial_infections(
 
     ##  Now make initial infections
     for _, agent in enumerate(initial_agents_to_infect):
-        new_state = np.random.randint(N_states - 1)  # E1-E4 or I1-I4, uniformly distributed
+        weights = calc_E_I_dist(my, 1)
+        states = np.arange(N_states - 1, dtype=np.int8)
+        new_state = nb_random_choice(states, weights)[0]  # E1-E4 or I1-I4, uniformly distributed
         my.state[agent] = new_state
 
         agents_in_state[new_state].append(np.uint32(agent))
@@ -1702,6 +1777,13 @@ def run_simulation(
         print("Where", where_infections_happened_counter)
         print("positive_test_counter", intervention.positive_test_counter)
         print("n_found", np.sum(np.array([1 for day_found in intervention.day_found_infected if day_found>=0])))
+        frac_inf = np.zeros((2,200))
+        for agent in range(my.cfg.N_tot):
+            n_con = my.number_of_contacts[agent]
+            frac_inf[1,n_con] +=1
+            if my.state[agent]>=0:
+                frac_inf[0,n_con] +=1
+        print(frac_inf[0,:]/frac_inf[1,:])
         # print("N_daily_tests", intervention.N_daily_tests)
         # print("N_positive_tested", N_positive_tested)
 
@@ -1778,7 +1860,7 @@ def initialize_tents(my, N_tents):
     for i in range(N_tents):
         tent_positions[i] = my.coordinates[np.random.randint(N_tot)]
 
-    tent_counter = np.zeros(N_tents, np.uint16)
+    tent_counter = np.zeros(N_tents, np.uint32)
     for agent in range(N_tot):
         distances = np.array(
             [utils.haversine_scipy(my.coordinates[agent], p_tent) for p_tent in tent_positions]
@@ -1794,7 +1876,7 @@ def initialize_tents(my, N_tents):
 def initialize_kommuner(my, df_coordinates):
     my.kommune = np.array(df_coordinates["idx"].values, dtype=np.uint8)
     kommune_counter = df_coordinates["idx"].value_counts().sort_index()
-    kommune_counter = np.array(kommune_counter, dtype=np.uint16)
+    kommune_counter = np.array(kommune_counter, dtype=np.uint32)
     return kommune_counter
 
 

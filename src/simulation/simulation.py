b3aea71983c1207d980904893c763119fcd96d01
ESMartiny <Emil.s.martiny@gmail.com>
2021-01-27 23:29:34 +0100

    lots of stuff, matrices kinda works
    Kinda works

--- src/simulation/simulation.py
+++ src/simulation/simulation.py
@@ -126,7 +126,6 @@ class Simulation:
             self.my.cfg.work_other_ratio = work_other_ratio
 
            
-            print(counter_ages)
             # work_other_ratio = 0.5  # 20% work, 80% other
 
             if self.verbose:
@@ -296,6 +295,11 @@ class Simulation:
         vaccinations_per_age_group, _, vaccination_schedule = utils.load_vaccination_schedule()
 
         # Convert vaccination_schedule to integer day counter
+        work_matrix_init, other_matrix_init, _, _= utils.load_contact_matrices()
+        work_matrix_restrict, other_matrix_restrict, _, _ = utils.load_contact_matrices()
+        work_matrix_restrict = work_matrix_restrict * 0.8 
+        other_matrix_restrict = other_matrix_restrict * 0.8
+
         # TODO: Use a better convertion method. --- Currently simluations start on 2020-12-28
         #print(vaccinations_per_age_group, vaccination_schedule)
         vaccinations_per_age_group=vaccinations_per_age_group.astype(np.int64)
@@ -306,6 +310,10 @@ class Simulation:
             labels = labels,
             vaccinations_per_age_group = vaccinations_per_age_group,
             vaccination_schedule = vaccination_schedule,
+            work_matrix_init = work_matrix_init,
+            work_matrix_restrict = work_matrix_restrict,
+            other_matrix_init = other_matrix_init,
+            other_matrix_restrict = other_matrix_restrict,
             verbose=verbose_interventions,
         )
         

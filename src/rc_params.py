f4f185cc03c819003a4483690e2f556de24cc89d
Rasmus Skytte Eriksen <RSKE@ssi.dk>
2021-02-03 15:49:41 +0100

    Small fixes, plotting now works on win10

--- src/rc_params.py
+++ src/rc_params.py
@@ -5,4 +5,5 @@ import matplotlib as mpl
 def set_rc_params(dpi=300):
     plt.rcParams["figure.figsize"] = (16, 10)
     plt.rcParams["figure.dpi"] = dpi
+    plt.rcParams["text.usetex"] = False
     mpl.rc("axes", edgecolor="k", linewidth=2)

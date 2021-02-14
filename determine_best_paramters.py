aba7de71fb36d06f679913cf3202e2e9b651708c
Rasmus Skytte Eriksen <rasmusskytteeriksen@gmail.com>
2021-02-14 15:07:06 +0100

    Improving the fitting workflow

--- determine_best_paramters.py
+++ determine_best_paramters.py
@@ -1,107 +1,55 @@
 import numpy as np
 import pandas as pd
 from datetime import datetime
-from scipy.stats import norm
 
 from tqdm import tqdm
 
-from importlib import reload
 from src.utils import utils
-from src import file_loaders
+from src.simulation import simulation
 from src import rc_params
+
 
 if utils.is_local_computer() :
     import matplotlib.pyplot as plt
-    import matplotlib.dates as mdates
+
+from contexttimer import Timer
+
+
+params, start_date = utils.load_params("cfg/simulation_parameters_fase_2.yaml")
+
+if utils.is_local_computer():
+    f = 0.1
+    #noise = lambda m, d : m
+    noise = lambda m, d : np.round(m + np.linspace(-d, d, 3), 5)
+    num_cores_max = 3
+else :
+    f = 0.5
+    noise = lambda m, d : np.round(m + np.linspace(-d, d, 5), 5)
+    num_cores_max = 20
+
+# Sweep around parameter set
+params["beta"]               = noise(params["beta"], 0.0005)
+params["beta_UK_multiplier"] = noise(params["beta_UK_multiplier"], 0.2)
+params["N_init"]             = noise(params["N_init"] * f, 2000 * f)
+params["N_init_UK_frac"]     = noise(params["N_init_UK_frac"], 0.01)
+
+# Scale the population
+params["N_tot"]  = int(params["N_tot"] * f)
+params["R_init"] = int(params["R_init"] * f)
+
+
+N_files_total = 0
+if __name__ == "__main__":
+    with Timer() as t:
+
+        N_files_total +=  simulation.run_simulations(params, N_runs=1, num_cores_max=num_cores_max)
+
+    print(f"\n{N_files_total:,} files were generated, total duration {utils.format_time(t.elapsed)}")
+    print("Finished simulating!")
 
 
 
-def aggregate_array(arr, chunk_size=10) :
-
-    # Get the average number per day
-    days = int(len(arr) / chunk_size)
-    for k in range(days) :
-        arr[k] = np.mean(arr[k*chunk_size:(k+1)*chunk_size])
-    return arr[:days]
-
-def load_from_file(filename) :
-
-    cfg = utils.read_cfg_from_hdf5_file(filename)
-
-    # Load the csv summery file
-    df = file_loaders.pandas_load_file(filename)
-
-    # Extract the values
-    I_tot    = df["I"].to_numpy() 
-    I_uk     = df["I^V_1"].to_numpy()
-
-    # Get daily averages
-    I_tot = aggregate_array(I_tot)
-    I_uk = aggregate_array(I_uk)
-
-    # Scale the number of infected
-    I_tot_scaled = I_tot / 2.7 * (5_800_000 / cfg.network.N_tot)
-
-    # Get the fraction of UK variants
-    with np.errstate(divide='ignore', invalid='ignore'):
-        f = I_uk / I_tot
-        f[np.isnan(f)] = -1
-
-    return(I_tot_scaled, f)
-
-
-def compute_likelihood(I_tot_scaled, f, index, fraction) :
-    
-    # Compute the likelihood
-    ll =  compute_loglikelihood_covid_index(I_tot_scaled, index)
-    ll += compute_loglikelihood_fraction_uk(f, fraction)
-
-    return ll
-
-
-def compute_loglikelihood_covid_index(I, index):
-
-    # Unpack values
-    covid_index, covid_index_sigma, covid_index_offset = index
-
-    if len(I) >= len(covid_index) + covid_index_offset :
-
-        # Get the range corresponding to the tests
-        I_model = I[covid_index_offset:covid_index_offset+len(covid_index)]
-
-        # Model input is number of infected. We assume 80.000 daily tests in the model
-        logK_model = np.log(I_model) - beta * np.log(80_000)
-
-        # Calculate (log) proability for every point
-        log_prop = norm.logpdf(logK_model, loc=covid_index, scale=covid_index_sigma)
-
-        # Determine the log likelihood
-        return np.sum(log_prop)
-
-    else :
-        return np.nan
-
-def compute_loglikelihood_fraction_uk(f, fraction):
-
-    # Unpack values
-    fraction, fraction_sigma, fraction_offset = fraction
-
-    # Get weekly values
-    f = aggregate_array(f, chunk_size=7)
-
-    if len(f) >= len(fraction) + fraction_offset :
-
-        # Get the range corresponding to the tests
-        fraction_model = f[fraction_offset:fraction_offset+len(fraction)]
-
-        # Calculate (log) proability for every point
-        log_prop = norm.logpdf(fraction_model, loc=fraction, scale=fraction_sigma)
-
-        # Determine the log likelihood
-        return np.sum(log_prop)
-
-    else :
-        return np.nan
+from src.analysis.helpers import *
 
 rc_params.set_rc_params()
 
@@ -120,7 +68,8 @@ logK       = df_index["logI"][ind:]     # Renaming the index I to index K to avo
 logK_sigma = df_index["logI_sd"][ind:]
 
 # Determine the covid_index_offset
-covid_index_offset = (datetime(2021, 1, 1) - datetime(2020, 12, 8)).days
+covid_index_offset = (datetime(2021, 1, 1) - datetime(2020, 12, 21)).days
+#covid_index_offset = (datetime(2021, 1, 1).date() - start_date).days
 
 fraction        = np.array([0.04,  0.074,  0.13,  0.2])
 fraction_sigma  = np.array([0.006, 0.0075, 0.015, 0.016])
@@ -146,7 +95,7 @@ for cfg in tqdm(
 
         # Evaluate
         tmp_ll = compute_likelihood(I_tot_scaled, f, 
-                                    (logK, logK_sigma, covid_index_offset), 
+                                    (logK, logK_sigma, covid_index_offset, beta), 
                                     (fraction, fraction_sigma, fraction_offset))
 
         ll.append(tmp_ll)
@@ -167,14 +116,14 @@ print(f"N_init_UK_frac : {cfg_best.N_init_UK_frac:.3f}")
 
 
 
-betas = np.array([cfg.beta for cfg in cfgs])
+betas     = np.array([cfg.beta               for cfg in cfgs])
 rel_betas = np.array([cfg.beta_UK_multiplier for cfg in cfgs])
 
-N_init = np.array([cfg.N_init for cfg in cfgs])
+N_init         = np.array([cfg.N_init         for cfg in cfgs])
 N_init_UK_frac = np.array([cfg.N_init_UK_frac for cfg in cfgs])
 
 best = lambda arr : np.array([np.mean(lls[arr == v]) for v in np.unique(arr)])
-err = lambda arr : np.array([np.std(lls[arr == v]) for v in np.unique(arr)])
+err  = lambda arr : np.array([np.std( lls[arr == v]) for v in np.unique(arr)])
 
 if not utils.is_local_computer() :
     fig, axes = plt.subplots(nrows=2, ncols=2, figsize=(12, 8))
@@ -196,24 +145,24 @@ if not utils.is_local_computer() :
     axes[3].scatter(cfg_best.N_init_UK_frac, ll_best, fmt="x")
     axes[3].set_xlabel('N_init_UK_frac')
 
-    plt.savefig('Figures/test.png')
+    plt.savefig('Figures/LogLikelihood_parameters.png')
 
-else :
-    def terminal_printer(name, arr, val) :
-    
-        I = np.argmax(np.unique(arr) == val)
-        out_string = "["
-        for i in range(len(arr)) :
-            if i == I :
-                out_string += " +"
-            else :
-                out_string += " -"
-        out_string += " ]"
-        print(name + "\t" + out_string)
 
-    print("--- Maximum likelihood value locations ---")
-    terminal_printer("beta* :    ", betas,          cfg_best.beta)
-    terminal_printer("rel_beta* :", rel_betas,      cfg_best.beta_UK_multiplier)
-    terminal_printer("N_init* :  ", N_init,         cfg_best.N_init)
-    terminal_printer("N_UK* :    ", N_init_UK_frac, cfg_best.N_init_UK_frac)
-    
+def terminal_printer(name, arr, val) :
+    u_arr = np.unique(arr)
+    I = np.argmax(u_arr == val)
+
+    out_string = "["
+    for i in range(len(u_arr)) :
+        if i == I :
+            out_string += " +"
+        else :
+            out_string += " -"
+    out_string += " ]"
+    print(name + "\t" + out_string)
+
+print("--- Maximum likelihood value locations ---")
+terminal_printer("beta* :    ", betas,          cfg_best.beta)
+terminal_printer("rel_beta* :", rel_betas,      cfg_best.beta_UK_multiplier)
+terminal_printer("N_init* :  ", N_init,         cfg_best.N_init)
+terminal_printer("N_UK* :    ", N_init_UK_frac, cfg_best.N_init_UK_frac)

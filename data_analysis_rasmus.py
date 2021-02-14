cd00a11a4ac96e9cf8ed663a8d121de442d9e783
Rasmus Skytte Eriksen <rasmusskytteeriksen@gmail.com>
2021-02-14 15:07:06 +0100

    Improving the fitting workflow

--- data_analysis_rasmus.py
+++ data_analysis_rasmus.py
@@ -2,7 +2,7 @@ from os import stat
 import numpy as np
 import pandas as pd
 from datetime import datetime
-from scipy.stats import norm
+
 
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
@@ -27,38 +27,6 @@ N = 25
 
 start_date = datetime(2020, 12, 21)
 
-def aggregate_array(arr, chunk_size=10) :
-
-    # Get the average number per day
-    days = int(len(arr) / chunk_size)
-    for k in range(days) :
-        arr[k] = np.mean(arr[k*chunk_size:(k+1)*chunk_size])
-    return arr[:days]
-
-def load_from_file(filename) :
-
-    cfg = utils.read_cfg_from_hdf5_file(filename)
-
-    # Load the csv summery file
-    df = file_loaders.pandas_load_file(filename)
-
-    # Extract the values
-    I_tot    = df["I"].to_numpy() 
-    I_uk     = df["I^V_1"].to_numpy()
-
-    # Get daily averages
-    I_tot = aggregate_array(I_tot)
-    I_uk = aggregate_array(I_uk)
-
-    # Scale the number of infected
-    I_tot_scaled = I_tot / 2.7 * (5_800_000 / cfg.network.N_tot)
-
-    # Get the fraction of UK variants
-    with np.errstate(divide='ignore', invalid='ignore'):
-        f = I_uk / I_tot
-        f[np.isnan(f)] = -1
-
-    return(I_tot_scaled, f)
 
 def plot_simulation(I_tot_scaled, f, start_date, axes) :
     
@@ -68,75 +36,8 @@ def plot_simulation(I_tot_scaled, f, start_date, axes) :
 
     return [tmp_handles_0, tmp_handles_2]
 
-def compute_likelihood(I_tot_scaled, f, index, fraction) :
-    
-    # Compute the likelihood
-    ll =  compute_loglikelihood_covid_index(I_tot_scaled, index)
-    ll += compute_loglikelihood_fraction_uk(f, fraction)
 
-    return ll
-
-def plot_daily_growth_rates(df, start_date, axes):
-
-    # Extract the values
-    I = np.array(df["I"]) / 2.7 * (5_800_000 / cfg.network.N_tot)
-
-    # Get the average number of infected per day
-    # (There are 10 clicks per day)
-    days = int(len(I) / 10)
-    for k in range(days) :
-        I[k] = np.mean(I[k*10:(k+1)*10])
-    I_m = I[:days]
-
-    # Plot the simulation prediction
-    lines.extend(axes.plot(pd.date_range(start=start_date, periods = len(I_m), freq="D"), I_m, lw = 4, c = "k"))
-
-    return lines
-
-def compute_loglikelihood_covid_index(I, index):
-
-    # Unpack values
-    covid_index, covid_index_sigma, covid_index_offset = index
-
-    if len(I) >= len(covid_index) + covid_index_offset :
-
-        # Get the range corresponding to the tests
-        I_model = I[covid_index_offset:covid_index_offset+len(covid_index)]
-
-        # Model input is number of infected. We assume 80.000 daily tests in the model
-        logK_model = np.log(I_model) - beta * np.log(80_000)
-
-        # Calculate (log) proability for every point
-        log_prop = norm.logpdf(logK_model, loc=covid_index, scale=covid_index_sigma)
-
-        # Determine the log likelihood
-        return np.sum(log_prop)
-
-    else :
-        return np.nan
-
-def compute_loglikelihood_fraction_uk(f, fraction):
-
-    # Unpack values
-    fraction, fraction_sigma, fraction_offset = fraction
-
-    # Get weekly values
-    f = aggregate_array(f, chunk_size=7)
-
-    if len(f) >= len(fraction) + fraction_offset :
-
-        # Get the range corresponding to the tests
-        fraction_model = f[fraction_offset:fraction_offset+len(fraction)]
-
-        # Calculate (log) proability for every point
-        log_prop = norm.logpdf(fraction_model, loc=fraction, scale=fraction_sigma)
-
-        # Determine the log likelihood
-        return np.sum(log_prop)
-
-    else :
-        return np.nan
-
+from src.analysis.helpers import *
 
 rc_params.set_rc_params()
 
@@ -197,13 +98,12 @@ for filename in tqdm(
 
     # Evaluate
     ll = compute_likelihood(I_tot_scaled, f, 
-                        (logK, logK_sigma, covid_index_offset), 
+                        (logK, logK_sigma, covid_index_offset, beta), 
                         (fraction, fraction_sigma, fraction_offset))
     
     # Store the plot handles and loglikelihoods
     plot_handles.append(h)
     lls.append(ll)
-
 
 # Filter out "bad" runs
 lls = np.array(lls)
@@ -249,7 +149,7 @@ axes[2].errorbar(t, fraction, yerr=fraction_sigma, fmt='s', lw=2)
 
 
 
-axes[0].set_ylim(0, 4000)
+#axes[0].set_ylim(0, 4000)
 axes[0].set_ylabel('Daglige positive')
 
 

Integer overflow error fixed for sogn. initializing at municipality level updated

src/simulation/nb_interventions.py
@@ -826,7 +826,7 @@ def apply_interventions_on_label(my, g, intervention, day, click, verbose=False)
                                 k = np.sum(intervention.cfg.threshold_interventions_to_apply[:i] == 3)
 
                                 if verbose :
-                                    if intervention.N_labels > 1 :
+                                    if intervention.N_matrix_labels > 1 :
                                         print('Intervention type : matrix restriction, name:', intervention.cfg.Intervention_contact_matrices_name[k] + '_label_' + str(ith_label) )
                                     else :
                                         print('Intervention type : matrix restriction, name:', intervention.cfg.Intervention_contact_matrices_name[k])

src/simulation/nb_network.py
@@ -555,7 +555,7 @@ def generate_one_household(people_in_household_sogn, agent, agent0, do_continue,
         #set age for agent
         age = age_index  # just use age index as substitute for age
         my.age[agent] = age
-        my.sogn[agent] = sogn
+        my.sogn[agent] = np.uint16(sogn)
         counter_ages[age_index] += 1
         agents_in_age_group[age_index].append(np.uint32(agent))
 